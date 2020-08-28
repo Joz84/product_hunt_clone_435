2c4a010dab8b07af8643622d7e47d531ba9c5463
jonathan serafini <jonathan.serafini@gmail.com>
2020-08-28 10:51:28 +0200

    add travis

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    return "#{capitalize_first_name} #{self.last_name.capitalize}"
+  end
+
+  private
+
+  def capitalize_first_name
+    "#{self.first_name.capitalize}"
+  end
 end
